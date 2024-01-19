1fb727494d1adb905f9b81574c15196f65a6b800
CFFC IT <rolandbitosjr@countryfunders.com>
2024-01-19 20:17:28 +0800

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,7 +3,6 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import useData from "../hooks/useData";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
@@ -15,7 +14,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
 import useData from "./useData";
 
 export interface Genres{
     id: number;
     name: string;
+    image_background: string;
 }
 
 const useGenres = () => useData<Genres>('/genres');
