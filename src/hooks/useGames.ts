558f36b611e2d600cd3dc5a757f623bc8be7177e
CFFC IT <rolandbitosjr@countryfunders.com>
2024-01-19 22:38:17 +0800

    Sort games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genres } from "./useGenres";
 
 export interface Platform{
     id: number;
@@ -17,7 +16,7 @@ export interface Game {
 
 const useGames = (gameQuery: GameQuery) => useData<Game>('/games',
   {params: {
-      genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id
+      genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder
     }
   }, [gameQuery]);
 
